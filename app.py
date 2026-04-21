40d4a924fdef9c05d8cb1802c35e6348ff7cd438
Kritika <kritikakartikey9@gmail.com>
2026-04-21 11:13:03 +0530

    ready for deploy

--- app.py
+++ app.py
@@ -377,3 +377,9 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
